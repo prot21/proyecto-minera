685ad38262e3c8e616c458148f3fdb0626dbcfd7
gera <j21nmc@gmail.com>
2022-06-02 00:06:52 -0500

    avance de Jorge

---
+++ WFC_Mina_Parcial/IService1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+//agregamos
+using System.Data;
+
+namespace DemoServiceInfracciones
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        DataSet ListarYacimiento();
+
+        [OperationContract]
+        DataSet InsertarYacimiento(String nombre, String idUbigeo, DateTime fecInicio, String estado, String usu_reg);
+
+
+        [OperationContract]
+        DataSet ActualizarYacimiento(String idYacimiento,  String nombre, String idUbigeo, DateTime fecInicio, String estado, String usuUltMod);
+
+        [OperationContract]
+        DataSet ConsultarYacimiento(String idYacimiento);
+
+        [OperationContract]
+        DataSet EliminarYacimiento(String idYacimiento);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    // Puede agregar archivos XSD al proyecto. Después de compilar el proyecto, puede usar directamente los tipos de datos definidos aquí, con el espacio de nombres "DemoServiceInfracciones.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WFC_Mina_Parcial/Service1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+//agregamos 
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DemoServiceInfracciones
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class Service1 : IService1
+    {
+        // los insumos ....
+        SqlConnection cnx = new SqlConnection(@"server=LAPTOP-CI3GTFUK;Database=Mineria;Integrated Security=SSPI");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter ada; 
+        DataSet dts = new DataSet();
+
+        DataSet IService1.ListarYacimiento()
+        {
+            try
+            {
+                cmd.Connection = cnx;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "usp_ListarYacimiento";
+                cmd.Parameters.Clear();
+                ada = new SqlDataAdapter(cmd);
+                ada.Fill(dts, "ListaYacimiento");
+                return dts;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+    DataSet IService1.InsertarYacimiento(String nombre, String idUbigeo, DateTime fecInicio, String estado, String usu_reg)
+    {
+        try
+        {
+            cmd.Connection = cnx;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "usp_InsertarYacimiento";
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@nombre", nombre);
+            cmd.Parameters.AddWithValue("@idUbigeo", idUbigeo);
+            cmd.Parameters.AddWithValue("@fecInicio", fecInicio);
+            cmd.Parameters.AddWithValue("@estado", estado);
+            cmd.Parameters.AddWithValue("@usu_reg", usu_reg);
+             
+                ada = new SqlDataAdapter(cmd);
+            ada.Fill(dts, "Insertar");
+            return dts;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+        DataSet IService1.ActualizarYacimiento(String idYacimiento, String nombre, String idUbigeo, DateTime fecInicio, String estado, String usuUltMod)
+        {
+            try
+            {
+                cmd.Connection = cnx;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "usp_ActualizarYacimiento";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@idYacimiento", idYacimiento);
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@idUbigeo", idUbigeo);
+                cmd.Parameters.AddWithValue("@fecInicio", fecInicio);
+                cmd.Parameters.AddWithValue("@estado", estado);
+                cmd.Parameters.AddWithValue("@usuUltMod", usuUltMod);
+
+                ada = new SqlDataAdapter(cmd);
+                ada.Fill(dts, "Actualizar");
+                return dts;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        DataSet IService1.ConsultarYacimiento(String idYacimiento)
+        {
+            try
+            {
+                cmd.Connection = cnx;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "usp_ConsultarYacimiento";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@idYacimiento", idYacimiento);
+
+                ada = new SqlDataAdapter(cmd);
+                ada.Fill(dts, "Consultar");
+                return dts;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        DataSet IService1.EliminarYacimiento(String idYacimiento)
+        {
+            try
+            {
+                cmd.Connection = cnx;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "usp_EliminarYacimiento";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@idYacimiento", idYacimiento);
+
+                ada = new SqlDataAdapter(cmd);
+                ada.Fill(dts, "eliminar");
+                return dts;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
